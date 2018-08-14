fc05086f9dac73aafc6ea71e5be942a07c855b7b
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-08-14 16:59:00 +0200

    added comments to dashboard-card

--- client/src/app/dashboard/dashboard-card/dashboard-card.component.ts
+++ client/src/app/dashboard/dashboard-card/dashboard-card.component.ts
@@ -7,33 +7,43 @@ import { InstanceService, Instance } from '../../instance-registry-service';
   styleUrls: ['./dashboard-card.component.css']
 })
 
+/**
+ * The dashboard-card component is used to provide
+ * an overview of the status of the given component.
+ * The needed information are querried based on the
+ * given componentType.
+ */
 export class DashboardCardComponent implements OnInit {
+
   @Input() img: string;
   @Input() route: string;
-  numberOfInstances: string;
-  numberOfFailedInstances: string;
   @Input() title: string;
   @Input() componentType: string;
 
+  numberOfInstances: string;
+  numberOfFailedInstances: string;
   private irService: InstanceService;
+
   constructor(irService: InstanceService) {
     this.irService = irService;
     this.numberOfFailedInstances = 'No server connection';
    }
 
   ngOnInit() {
-    this.queryInstanceNumber();
+    // this has to be called onInit and not in the constructor, due
+    // to the component livecycle. Input's are not initialized in
+    // the constructor.
+    this.setInstanceNumber();
   }
 
-  private queryInstanceNumber() {
-    // TODO: call the generated swagger code to get the
-    // number of instances. Make it fault prove, no server connection
-    // should result in a default display and should not cause
-    // the application to crash
+  /**
+   * Uses the instance service to query the number of instances in the current system.
+   * If there is no server connection the value is set to a default error message.
+   */
+  private setInstanceNumber() {
     this.irService.getInstanceNumber(this.componentType).subscribe((amount: number) => {
       this.numberOfInstances = '' + amount;
     }, (error) => {
-      // console.log('currently there is no server connection. error:', error);
       this.numberOfInstances = 'No server connection';
     });
   }
