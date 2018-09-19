7aae701f491ac780095e086b7981a1f0b54250bc
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-09-19 13:38:31 +0200

    removed further unfinished functionality. added a user profile page at the appropiate place as a placehodler

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -23,6 +23,7 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardOverviewComponent } from './dashboard-overview/dashboard-overview.component';
 import { CrawlerComponent } from './crawler/crawler.component';
 import { WebApiComponent } from './webapi/web-api.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
 
 const dashboardRoutes: Routes = [
   {
@@ -44,7 +45,11 @@ const dashboardRoutes: Routes = [
    {
      path: 'webApi',
      component: WebApiComponent
-   }
+   },
+    {
+      path: 'userProfile',
+      component: UserProfileComponent
+    }
   ]
 }];
 
