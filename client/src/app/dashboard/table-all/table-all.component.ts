4b2619fa875ecce61dcb572bd408b763ad6b1db7
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-09-19 13:17:06 +0200

    removed unfinished and experimental features to prepare the pull request to develop

--- client/src/app/dashboard/table-all/table-all.component.ts
+++ client/src/app/dashboard/table-all/table-all.component.ts
@@ -31,15 +31,13 @@ import { DeletedialogComponent } from '../deletedialog/deletedialog.component';
 export class TableAllComponent implements OnInit {
 
   @Input() set data_array(data_array: Instance[]) {
-    console.log('here with data_array', data_array);
     if (this.dataSource != null) {
       this.dataSource = new MatTableDataSource<Instance>(data_array);
-      console.log('created data source', this.dataSource);
     } else {
       this.dataSource.data = data_array;
     }
   }
-  displayedColumns = ['ID', 'name', 'host', 'portNumber', 'action', 'select'];
+  displayedColumns = ['ID', 'name', 'host', 'portNumber', 'select'];
   dataSource: MatTableDataSource<Instance> = new MatTableDataSource<Instance>(this.data_array);
   selection = new SelectionModel<Instance>(true, []);
   dialogResult: any;
