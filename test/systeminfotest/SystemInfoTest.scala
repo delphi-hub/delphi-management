db37fcc950b4d00c472b2e46d8a0023e8a336d46
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-09-26 13:40:58 +0200

    resolved merge conflict

--- test/systeminfotest/SystemInfoTest.scala
+++ test/systeminfotest/SystemInfoTest.scala
@@ -18,23 +18,6 @@
 
 package systeminfotest
 
-<<<<<<< HEAD
-import org.scalatest.FlatSpec
-import controllers.{HostName, JavaVersion, ScalaVersion}
-
-class SystemInfoTest  extends FlatSpec {
-  it should "contains non empty java version" in {
-    assertResult(false)(Option(JavaVersion(None)).isEmpty)
-  }
-  it should "contains non empty scala version" in {
-    assertResult(false)(Option(ScalaVersion(None)).isEmpty)
-  }
-  it should "contains non empty hostname" in {
-    assertResult(false)(Option(HostName(None)).isEmpty)
-  }
-  it should "contains non empty platform value" in {
-    assertResult(false)(Option(JavaVersion(None)).isEmpty)
-=======
 import controllers.SystemInfoController
 import models.SystemInfo
 
@@ -63,7 +46,6 @@ class SystemInfoTest extends PlaySpec with Results {
       validateResult.isSuccess mustBe true
 
     }
->>>>>>> feature/angularIntegration
   }
 
 }
