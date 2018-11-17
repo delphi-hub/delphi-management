8bf6355c0d2bf48cc479c361d46b3d717e73ca5d
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-11-17 18:32:30 +0100

    codestyle

--- client/src/karma.conf.js
+++ client/src/karma.conf.js
@@ -46,8 +46,8 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
       }
     },
     singleRun: true
