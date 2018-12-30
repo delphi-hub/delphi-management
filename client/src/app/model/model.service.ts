f2f1b3bab16e1a3e0da4c3213140d24815c2b956
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2018-12-30 21:04:06 +0100

    commented the model service

--- client/src/app/model/model.service.ts
+++ client/src/app/model/model.service.ts
@@ -10,13 +10,26 @@ import {BehaviorSubject, Observable} from 'rxjs';
 export interface InstanceChange extends Change {
   allInstances?: { [id: number]: Instance };
 }
+
+/**
+ * This service is used to further abstract the acess to any data provided
+ * by @class ApiService and @class SocketService.
+ * The programmer should only interact with this class and the @class StoreService
+ * in order to receive information about the current state of the application.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ModelService {
 
   private readonly instanceSubject: BehaviorSubject<InstanceChange>;
+  private readonly updateCycle = 30000;
 
+  /**
+   * Subscribes to state updates at the @class StoreService and notifies every subscriber of the this service if
+   * a change to the store happens.
+   * Also periodically queries the ApiService every 30000ms (set in the updateCycle variable) for updates.
+   */
   constructor(private socketService: SocketService, private apiService: ApiService, private storeService: StoreService) {
     this.instanceSubject = new BehaviorSubject<InstanceChange>({type: Actions.NONE});
 
@@ -28,17 +41,21 @@ export class ModelService {
 
       this.initInstances();
 
-
       setInterval(() => {
         this.updateAllInstances(true);
-      }, 30000);
+      }, updateCycle);
     });
   }
 
+  /**
+   * Used to query all network information from the api service.
+   * Optionally a @param calculateDiff can be given to the @class StoreService
+   * to calculate a diff between the old and new state before applying an update.
+   */
   private updateAllInstances(calculateDiff= false) {
     // get current instances and update the current state
     this.apiService.getInstanceNetwork().subscribe((network: Array<Instance>) => {
-      console.log('received instacne response', network);
+      console.log('received instance response', network);
       // parse network to state and update after diff
       this.storeService.addInstancesToState(network, calculateDiff);
     }, (error) => {
@@ -46,6 +63,10 @@ export class ModelService {
     });
   }
 
+  /**
+   * Updates the state after receiving all instances from the api service.
+   * Subscribes to InstanceAdded and InstanceRemoved Events at the socket service.
+   */
   private initInstances() {
     this.updateAllInstances();
     // register for event updates
@@ -63,6 +84,10 @@ export class ModelService {
     // this.socketService.subscribeForEvent<>(EventTypeEnum.LinkStateChangedEvent).subscribe(() => {});
   }
 
+  /**
+   * Returns an observable, which notifies every subscriber about changes to the
+   * instance stored in the state.
+   */
   public getObservableForInstances() {
     return new Observable<InstanceChange>((observer) => {
       this.instanceSubject.subscribe(observer);
@@ -75,6 +100,9 @@ export class ModelService {
     });
   }
 
+  /**
+   * Returns all instances of the given @params compType.
+   */
   public getComponentsByType(compType: ComponentType) {
     const state = this.storeService.getState();
     return state.instancesByType[compType].map((id) => {
