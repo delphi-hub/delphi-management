becbabdebd27e407a47aa4da9b3253c2b1178b6a
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2019-02-19 16:18:04 +0100

    fixed auth service test setup. cleaned some unused imports from test cases.

--- client/src/app/api/auth.service.spec.ts
+++ client/src/app/api/auth.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { ApiService } from './api/api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({providers: [ApiService], imports: [HttpClientTestingModule]}));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
