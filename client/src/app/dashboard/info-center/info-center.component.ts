7d7682cc6143ff97e9c8f7a1404d6e9f7301d15e
Unknown <pobouh.winnie@yahoo.fr>
2019-02-21 20:18:24 +0100

    review code

--- client/src/app/dashboard/info-center/info-center.component.ts
+++ client/src/app/dashboard/info-center/info-center.component.ts
@@ -1,10 +1,10 @@
 import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import { MatPaginator, MatSort, MatTable } from '@angular/material';
 import { InfoCenterDataSource } from './info-center-datasource';
-import {SocketService} from "../../api/api/socket.service";
-import {DatePipe} from "@angular/common";
-import {EventTypeEnum} from "../../model/models/socketMessage";
-import {Instance} from "../../model/models/instance";
+import {SocketService} from '../../api/api/socket.service';
+import {DatePipe} from '@angular/common';
+import {EventTypeEnum} from '../../model/models/socketMessage';
+import {Instance} from '../../model/models/instance';
 
 @Component({
   selector: 'app-info-center',
@@ -20,32 +20,34 @@ export class InfoCenterComponent implements OnInit {
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['type', 'notifName', 'dateTime', 'details'];
 
-  constructor(private socketService: SocketService, private changeDetectorRefs: ChangeDetectorRef){}
-
+  constructor(private socketService: SocketService, private changeDetectorRefs: ChangeDetectorRef) {}
   ngOnInit() {
     this.dataSource = new InfoCenterDataSource(this.paginator, this.sort);
     this.socketService.subscribeForEvent<Instance>(EventTypeEnum.InstanceAddedEvent).subscribe((newInstance: Instance) => {
-      var datePipe = new DatePipe("en-US");
-      var actualDate = datePipe.transform(Date.now(), 'dd/MM/yyyy hh:mm:ss:SSS');
-      let line = {instanceId: newInstance.id, type: 'add_circle', notifName: 'new Instance added', dateTime: actualDate, details: 'Instance Name: '+newInstance.name};
+      const datePipe = new DatePipe('en-US');
+      const actualDate = datePipe.transform(Date.now(), 'dd/MM/yyyy hh:mm:ss:SSS');
+      const line = {instanceId: newInstance.id, type: 'add_circle',
+        notifName: 'new Instance added', dateTime: actualDate, details: 'Instance Name: ' + newInstance.name};
       this.dataSource.add(line);
       this.changeDetectorRefs.detectChanges();
       this.paginator.lastPage();
       this.paginator.firstPage();
     });
     this.socketService.subscribeForEvent<Instance>(EventTypeEnum.InstanceRemovedEvent).subscribe((removeInstance: Instance) => {
-      var datePipe = new DatePipe("en-US");
-      var actualDate = datePipe.transform(Date.now(), 'dd/MM/yyyy hh:mm:ss:SSS');
-      let line = {instanceId: removeInstance.id, type: 'delete_sweep', notifName: 'Instance removed', dateTime: actualDate, details: 'Instance Name: '+removeInstance.name};
+      const datePipe = new DatePipe('en-US');
+      const actualDate = datePipe.transform(Date.now(), 'dd/MM/yyyy hh:mm:ss:SSS');
+      const line = {instanceId: removeInstance.id, type: 'delete_sweep',
+        notifName: 'Instance removed', dateTime: actualDate, details: 'Instance Name: ' + removeInstance.name};
       this.dataSource.add(line);
       this.changeDetectorRefs.detectChanges();
       this.paginator.lastPage();
       this.paginator.firstPage();
     });
     this.socketService.subscribeForEvent<Instance>(EventTypeEnum.LinkStateChangedEvent).subscribe((changeInstance: Instance) => {
-      var datePipe = new DatePipe("en-US");
-      var actualDate = datePipe.transform(Date.now(), 'dd/MM/yyyy hh:mm:ss:SSS');
-      let line = {instanceId: changeInstance.id, type: 'link', notifName: 'Instance changed', dateTime: actualDate, details: changeInstance.name};
+      const datePipe = new DatePipe('en-US');
+      const actualDate = datePipe.transform(Date.now(), 'dd/MM/yyyy hh:mm:ss:SSS');
+      const line = {instanceId: changeInstance.id, type: 'link',
+        notifName: 'Instance changed', dateTime: actualDate, details: changeInstance.name};
       this.dataSource.add(line);
       this.changeDetectorRefs.detectChanges();
       this.paginator.lastPage();
