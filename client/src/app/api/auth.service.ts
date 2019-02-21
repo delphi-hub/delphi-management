42fd5badd4b2222602236b440821422ce66cca12
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2019-02-21 09:20:26 +0100

    basic is valid and get token functionality

--- client/src/app/api/auth.service.ts
+++ client/src/app/api/auth.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api/api.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HashLocationStrategy } from '@angular/common';
 
+const TOKEN_IDENT = 'token';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private helperService: JwtHelperService;
   constructor(private apiService: ApiService) {
-   }
+    this.helperService = new JwtHelperService();
+  }
 
   login(username: string, password: string) {
     this.apiService.login(username, password).subscribe((token: string) => {
       console.log('got token', token);
-      localStorage.setItem('id_token', token);
+      localStorage.setItem(TOKEN_IDENT, token);
     });
+  }
+
+  isValid(): boolean {
+    return this.helperService.isTokenExpired(this.getToken());
+  }
+
+  getToken(): string {
+    return localStorage.getItem(TOKEN_IDENT);
   }
 }
